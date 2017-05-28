864e5192b65027e080a8a86278d3638f66011956
Piotr Muzyka <piotrmuzyka@yahoo.com>
2017-05-28 23:42:06 +0200

    initial commit.

---
+++ app/src/main/java/com/example/piotrm/hatshop/HatShop.java
@@ -0,0 +1,13 @@
+package com.example.piotrm.hatshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HatShop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hat_shop);
+    }
+}
